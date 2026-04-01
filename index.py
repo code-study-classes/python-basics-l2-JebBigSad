5bb9457ffbaaae7bfae6187ac5e7c3659fca3117
JebBigSad <klimhonr.ru@gmail.com>
2026-04-01 08:50:26 +0000

    ura3

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ def get_domains(emails):
 # 3===============
 
 def filter_target_audience(users):
-    return list(filter(lambda user: user['age'] >= 18 and user['is_premium'],uers))
+    return list(filter(lambda user: user['age'] >= 18 and user['is_premium'],users))
 
 # 4========================
 
